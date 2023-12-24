dfc7055838d8418c2fe4a491469d52f8d31c7e4a
Juni Kim <junikimm717@gmail.com>
2023-12-24 16:58:07 -0500

    new contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  site: 'https://junickim.me',
+  integrations: [tailwind(), react(), mdx(), sitemap()],
   vite: {
     ssr: {
-      noExternal: ["react-icons"],
-    },
+      noExternal: ["react-icons"]
+    }
   },
   markdown: {
     shikiConfig: {
       experimentalThemes: {
-        light: 'light-plus',
-        dark: 'one-dark-pro',
-      },
+        light: 'min-light',
+        dark: 'one-dark-pro'
+      }
     },
-  },
+  }
 });
