ff033e6037c7ae3e3c8fbdf6ade9cb2b27e275ce
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2023-12-26 09:19:35 -0500

    partytown and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,29 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://junickim.me",
-  integrations: [tailwind(), react(), mdx(), sitemap()],
+  integrations: [tailwind(), react(), mdx(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })],
   vite: {
     ssr: {
-      noExternal: ["react-icons"],
-    },
+      noExternal: ["react-icons"]
+    }
   },
   markdown: {
     shikiConfig: {
       experimentalThemes: {
         light: "rose-pine-dawn",
-        dark: "rose-pine-moon",
-      },
-    },
-  },
+        dark: "rose-pine-moon"
+      }
+    }
+  }
 });
