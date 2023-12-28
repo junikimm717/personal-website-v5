3b96391fe64fa21cdc2921ad5e3dbfbaaa696552
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2023-12-27 22:34:05 -0500

    patched navbar to not flicker on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,19 @@ export default function Navbar(props: { children: React.ReactNode }) {
   return small ? (
     <div id="navone" className="column mx-1 p-3">
       <div className="flex flex-row justify-between">
-        <h1 className="text-lg font-extrabold">Juni C. Kim</h1>
+        <h1 className="text-xl font-extrabold">Juni C. Kim</h1>
         <button
-          className="text-xl border border-gray-400 dark:border-gray-600 px-3 group"
+          className="text-xl border border-gray-400 dark:border-gray-600 px-3 flex twocol:hidden items-center justify-center"
           aria-label="Toggle Navbar Items"
           onClick={() => setOpen(!open)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="16"
-            width="16"
+            height="14"
+            width="14"
             viewBox="0 0 512 512"
             className={
-              "dark:fill-white fill-black duration-500" +
+              "dark:fill-white fill-black transition-all duration-500" +
               " " +
               (open ? "rotate-180" : "rotate-0")
             }
@@ -33,7 +33,28 @@ export default function Navbar(props: { children: React.ReactNode }) {
     </div>
   ) : (
     <div id="navtwo" className="column mx-1 p-3 top-6 sticky">
-      <h1 className="text-xl font-extrabold">Juni C. Kim</h1>
+      <div className="flex flex-row justify-between">
+        <h1 className="text-xl font-extrabold">Juni C. Kim</h1>
+        <button
+          className="text-xl border border-gray-400 dark:border-gray-600 px-3 flex twocol:hidden items-center justify-center"
+          aria-label="Toggle Navbar Items"
+          onClick={() => setOpen(!open)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="14"
+            width="14"
+            viewBox="0 0 512 512"
+            className={
+              "dark:fill-white fill-black transition-all duration-500" +
+              " " +
+              (open ? "rotate-180" : "rotate-0")
+            }
+          >
+            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+          </svg>
+        </button>
+      </div>
       <div className="hidden twocol:block font-mono">{props.children}</div>
     </div>
   );
