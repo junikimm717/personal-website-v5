bed989a78e5195e25681fb5ab942f4ebe8d0c986
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2023-12-27 19:46:21 -0500

    fixed darkmode component

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -10,21 +10,23 @@ export default function DarkMode() {
       setDark(false);
     }
   }, []);
-  useEffect(() => {
-    if (dark) {
+  const update = () => {
+    if (!dark) {
+      setDark(!dark);
       document.documentElement.classList.add("dark");
       localStorage.setItem("dark", "true");
     } else {
+      setDark(!dark);
       document.documentElement.classList.remove("dark");
       localStorage.setItem("dark", "false");
     }
-  }, [dark]);
+  };
   return (
     <div className="w-full flex justify-center">
       <button
         className="duration-500 text-2xl border border-gray-400 dark:border-gray-600 hover:bg-darkbg hover:text-white dark:hover:bg-white dark:hover:text-black p-2 mx-auto mt-2 w-12 h-12 flex justify-center items-center"
         aria-label="Toggle Dark Mode"
-        onClick={() => setDark(!dark)}
+        onClick={update}
       >
         {dark ? <FaRegSun /> : <FaRegMoon />}
       </button>
