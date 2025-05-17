fa6e5c3829dcb11a969248c9a5fd07a2a38fbee4
Juni Kim <junikimm717@gmail.com>
2025-05-17 15:47:18 -0400

    better i18n

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -7,7 +7,7 @@ export default function Settings(props: { lang: "en" | "ko" }) {
       <span className="flex gap-2 justify-center">
         <button
           className="flex justify-center items-center p-2 mx-auto mt-2 w-12 h-12 text-2xl no-underline border border-gray-400 duration-500 dark:border-gray-600 hover:text-white dark:hover:bg-white dark:hover:text-black hover:bg-darkbg"
-          onClick={() => navigate(lang === "ko" ? "/" : "/ko")}
+          onClick={() => navigate(lang === "ko" ? "/en" : "/ko")}
         >
           {lang === "ko" ? "en" : "한"}
         </button>
